a4cd54c80271235b8f016627bc2641221f9be348
Enrico Gervacio <gervacio.enrico@gmail.com>
2023-01-03 14:28:22 +0800

    Add Header styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+
+import Header from './Components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Enrico Gervacio</h1>
-    </div>
+
+    <div className="App" >
+      <Header text='Feedback UI' />
+    </div >
+
   )
 }
 
