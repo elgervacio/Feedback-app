a556942c8bcc2c8020ebe7e5cf83c3a338e27fda
Enrico Gervacio <gervacio.enrico@gmail.com>
2023-01-04 14:48:14 +0800

    added styles in FeedbackForm components

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react'
+import Button from './shared/Button'
 import Card from './shared/Card'
 
 const FeedbackForm = () => {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
     const handleTextChange = (e) => {
+        if (text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if (text !== '' && text.trim().length <= 10) {
+            setMessage('Text must be at least 10 characters!')
+            setBtnDisabled(true)
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setText(e.target.value)
     }
     return (
@@ -14,8 +27,10 @@ const FeedbackForm = () => {
                     <input onChange={handleTextChange} type="text"
                         placeholder='Write areview'
                         value={text} />
-                    <button type="submit">Send</button>
+                    <Button type="submit"
+                        isDisabled={btnDisabled}>Send</Button>
                 </div>
+                {message && <div className='message'>{message}</div>}
             </form>
         </Card>
     )
